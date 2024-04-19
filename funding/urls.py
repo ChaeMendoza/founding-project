a52aecbd793430ca5faff48d732387d74e9c816e
pollct <poll.cedeno@outlook.com>
2024-04-19 18:43:00 -0500

    Update project

--- funding/urls.py
+++ funding/urls.py
@@ -1,18 +1,7 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from datetime import datetime
-from django.contrib import messages
-import json
-from django.http import JsonResponse
+from django.urls import path
+from . import views
 
-# Create your views here.
-from django.shortcuts import render
-from funding.models import Categoria
-
-def home(request):
-   
-   return render(request,'index.html')
-
-def do_categorias(request):
-    categoria=Categoria.objects.all()
-    return render(request,'campanias.html',{'categorias':categoria})
+urlpatterns = [    
+    path('',views.home,name='home'),
+    #path('categorias/',views.do_categorias,name='categorias')
+]
--- funding/views.py
+++ funding/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+   return render(request,'index.html')
+
 def home_view(request):
     return render(request, 'funding/home.html')
 
