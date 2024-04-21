8319dab50de69b6953d3e5570242f25904321dff
Israel Mendoza <israel@dynasoft.ec>
2024-04-20 21:30:10 -0500

    Add: Login and register functions only

--- funding/views.py
+++ funding/views.py
@@ -1,23 +1,39 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, logout
+from django.contrib.auth import authenticate, login, logout
+from .forms import SignupForm, LoginForm
 
 # Create your views here.
 
 def home_view(request):
     return render(request, 'funding/home.html')
 
-#def login_view(request):
-#    form = AuthenticationForm(request, data=request.POST)
-#    if form.is_valid():
-#        user = form.get_user()
-#        login(rquest, user)
-#        return redirect('home')
-#    else:
-#        form = AuthenticationForm()
-#    return render(request, 'founding/login.html', {'form': form})
-def login_view(request):
-    return render(request, 'funding/login.html')
-
 def register_view(request):
-    return render(request, 'funding/register.html')
+    form = SignupForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            form = SignupForm()
+    return render(request, 'funding/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = LoginForm()        
+    return render(request, 'funding/login.html', {'form': form})
+
+# logout  page
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
